874ebdd8795a3217cb3941b9d07afef9e36c93f0
RizvanVeliyev <veliyevrizvan250@gmail.com>
2025-01-07 09:59:02 +0400

    CloudinaryService done, packages realted cloudinaryinstalled, Cloudinaryoptionsdto done, and in appsetting.json, CloudinarySettings key

---
+++ Restaurant.BLL/Dtos/AppSettingsDto/CloudinaryOptionsDto.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.BLL.Abstractions.Dtos;
+
+namespace Restaurant.BLL.Dtos.AppSettingsDto
+{
+
+    public class CloudinaryOptionsDto : IDto
+    {
+        public string APIKey { get; set; } = null!;
+        public string APISecret { get; set; } = null!;
+        public string CloudName { get; set; } = null!;
+
+    }
+}
