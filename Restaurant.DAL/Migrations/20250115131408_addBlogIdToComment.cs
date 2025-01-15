1304cf8cd8efc72207482f6b1a61dc4fe4a62c6a
RizvanVeliyev <veliyevrizvan250@gmail.com>
2025-01-15 18:06:19 +0400

    working on Comment

---
+++ Restaurant.DAL/Migrations/20250115131408_addBlogIdToComment.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBlogIdToComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_CartId_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "CartItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carts_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId_ProductId",
+                table: "CartItems",
+                columns: new[] { "CartId", "ProductId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_AppUserId",
+                table: "Carts",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Restaurant.DAL/Repositories/Abstractions/ICommentRepository.cs
+++ Restaurant.DAL/Repositories/Abstractions/ICommentRepository.cs
@@ -1,10 +1,5 @@
 ﻿using Restaurant.Core.Entities;
 using Restaurant.DAL.Repositories.Abstractions.Generic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Restaurant.DAL.Repositories.Abstractions
 {
