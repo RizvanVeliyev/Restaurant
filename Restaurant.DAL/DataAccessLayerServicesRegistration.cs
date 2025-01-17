4a424e88f1bd5cc39c4233b527e284abba24057e
RizvanVeliyev <veliyevrizvan250@gmail.com>
2025-01-17 17:10:00 +0400

    UI updated with resources

--- Restaurant.DAL/DataAccessLayerServicesRegistration.cs
+++ Restaurant.DAL/DataAccessLayerServicesRegistration.cs
@@ -69,6 +69,7 @@ namespace Restaurant.DAL
             services.AddSingleton<ShopLocalizer>();
             services.AddSingleton<BlogLocalizer>();
             services.AddSingleton<MenuLocalizer>();
+            services.AddSingleton<AccountLocalizer>();
 
         }
 
---
+++ Restaurant.DAL/Localizers/AccountLocalizer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Text;
+
+namespace Restaurant.DAL.Localizers
+{
+    public class AccountLocalizer
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public AccountLocalizer(IStringLocalizerFactory factory)
+        {
+            _localizer = factory.Create("Account", "Restaurant");
+        }
+
+        public string GetValue(string key)
+        {
+            return _localizer.GetString(key);
+        }
+    }
+}
