cdbd71479bc109af8f6ca1dac229b2e858aa3981
RizvanVeliyev <veliyevrizvan250@gmail.com>
2025-01-17 18:34:27 +0400

    Resources with UI done, Only Layout need changes

---
+++ Restaurant.DAL/Localizers/ReservationLocalizer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace Restaurant.DAL.Localizers
+{
+    public class ReservationLocalizer
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public ReservationLocalizer(IStringLocalizerFactory factory)
+        {
+            _localizer = factory.Create("Reservation", "Restaurant");
+        }
+
+        public string GetValue(string key)
+        {
+            return _localizer.GetString(key);
+        }
+    }
+}
